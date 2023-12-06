d02c9fba7b340c4245f6267a7db5b18523d41d17
iulian0585 <iam.constantinescu@gmail.com>
2023-12-06 17:49:17 +0200

    use the levinineiasi account as source

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 module "aft" {
-  source = "git@github.com:iulian0585/terraform-aws-control_tower_account_factory.git?ref=1.10.4-1"
+  source = "git@github.com:levinineiasi/lz-terraform-aws-control_tower_account_factory.git?ref=1.10.4-1"
 #  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory.git?ref=1.10.4"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
@@ -12,10 +12,10 @@ module "aft" {
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/terraform-aft-account-customizations"
+  account_request_repo_name                     = "${var.github_username}/lz-terraform-aft-account-request"
+  account_provisioning_customizations_repo_name = "${var.github_username}/lz-terraform-aft-account-provisioning-customizations"
+  global_customizations_repo_name               = "${var.github_username}/lz-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "${var.github_username}/lz-terraform-aft-account-customizations"
 
   aft_vpc_endpoints = false
 }
